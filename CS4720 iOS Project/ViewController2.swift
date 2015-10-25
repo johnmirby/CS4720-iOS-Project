be0da04def424264a28ae8810e025d8eb7478c57
John Irby <johnmirby@gmail.com>
2015-10-25 02:45:11 -0400

    Added user info screen

---
+++ CS4720 iOS Project/ViewController2.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController2.swift
+//  CS4720 iOS Project
+//
+//  Created by John Irby on 10/25/15.
+//  Copyright © 2015 John Irby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
