ac45a13d8ebcaecfc500707fc5361d3580ff14f7
Kyle Eklund <kae2k@labmini03.local>
2015-10-27 00:37:06 -0400

    Added taking an image in the detail view

--- CS4720 iOS Project/ViewController4.swift
+++ CS4720 iOS Project/ViewController4.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController4: UIViewController {
+class ViewController4: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
     
     var nameToDisplay = ""
+    var imagePicker: UIImagePickerController!
     
     @IBOutlet var label: UILabel!
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +31,19 @@ class ViewController4: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addImage(sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
     }
     
 
