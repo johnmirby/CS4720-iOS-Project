6ffb3e64b963352f9ff7a46e8055ffdf432a43c8
John Irby <johnmirby@gmail.com>
2015-11-02 17:03:04 -0500

    Added GPS functionality to add map markers in list item view

--- CS4720 iOS Project/ViewController4.swift
+++ CS4720 iOS Project/ViewController4.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController4: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+class ViewController4: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, CLLocationManagerDelegate  {
+    
+    var locationManager: CLLocationManager?
     
     var nameToDisplay = ""
     var imagePicker: UIImagePickerController!
@@ -19,6 +22,114 @@ class ViewController4: UIViewController, UINavigationControllerDelegate, UIImage
     @IBOutlet var label: UILabel!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet weak var mapView: MKMapView!
+    
+    
+    @IBAction func updateMarkerLocation(sender: AnyObject) {
+        let point = MKPointAnnotation()
+        if (locationManager?.location != nil){
+            point.coordinate = (locationManager?.location?.coordinate)!
+            mapView.addAnnotation(point)
+        }
+        else {
+            displayAlertWithTitle("No Current Location", message: "Please wait for the current location to be updated.")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count == 0{
+            //handle error here
+            return
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager,
+        didFailWithError error: NSError){
+            print("Location manager failed with error = \(error)")
+    }
+    
+    func locationManager(manager: CLLocationManager,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus){
+            
+            print("The authorization status of location services is changed to: ", terminator: "")
+            
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                print("Authorized")
+            case .AuthorizedWhenInUse:
+                print("Authorized when in use")
+            case .Denied:
+                print("Denied")
+            case .NotDetermined:
+                print("Not determined")
+            case .Restricted:
+                print("Restricted")
+            }
+            
+    }
+    
+    func displayAlertWithTitle(title: String, message: String){
+        let controller = UIAlertController(title: title,
+            message: message,
+            preferredStyle: .Alert)
+        
+        controller.addAction(UIAlertAction(title: "OK",
+            style: .Default,
+            handler: nil))
+        
+        presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    
+    func createLocationManager(startImmediately startImmediately: Bool){
+        locationManager = CLLocationManager()
+        if let manager = locationManager{
+            print("Successfully created the location manager")
+            manager.delegate = self
+            if startImmediately{
+                manager.startUpdatingLocation()
+            }
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /* Are location services available on this device? */
+        if CLLocationManager.locationServicesEnabled(){
+            
+            /* Do we have authorization to access location services? */
+            switch CLLocationManager.authorizationStatus(){
+            case .AuthorizedAlways:
+                /* Yes, always */
+                createLocationManager(startImmediately: true)
+            case .AuthorizedWhenInUse:
+                /* Yes, only when our app is in use */
+                createLocationManager(startImmediately: true)
+            case .Denied:
+                /* No */
+                displayAlertWithTitle("Not Determined",
+                    message: "Location services are not allowed for this app")
+            case .NotDetermined:
+                /* We don't know yet, we have to ask */
+                createLocationManager(startImmediately: false)
+                if let manager = self.locationManager{
+                    manager.requestWhenInUseAuthorization()
+                }
+            case .Restricted:
+                /* Restrictions have been applied, we have no access
+                to location services */
+                displayAlertWithTitle("Restricted",
+                    message: "Location services are not allowed for this app")
+            }
+            
+            
+        } else {
+            /* Location services are not enabled.
+            Take appropriate action: for instance, prompt the
+            user to enable the location services */
+            print("Location services are not enabled")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,11 +174,6 @@ class ViewController4: UIViewController, UINavigationControllerDelegate, UIImage
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
-        
-        let point = MKPointAnnotation()
-        point.coordinate = CLLocationCoordinate2D(latitude: 38.0350, longitude: -78.5050)
-        point.title = "University of Virginia"
-        mapView.addAnnotation(point)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
