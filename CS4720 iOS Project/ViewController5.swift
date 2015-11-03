aa708652e6c4104c5e0742fdebc41ca1d0da1342
Kyle Eklund <kae2k@labmini03.local>
2015-11-02 21:20:40 -0500

    Can now add items to custom list

---
+++ CS4720 iOS Project/ViewController5.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController5.swift
+//  CS4720 iOS Project
+//
+//  Created by Kyle Eklund on 11/2/15.
+//  Copyright © 2015 John Irby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController5: UIViewController {
+    
+    var tableData = ["1. Sample List Item"]
+    var valueToPass:String!
+    var listCount = 1
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "detailSegue"){
+            if let svc = segue.destinationViewController as? ViewController4 {
+                svc.nameToDisplay = valueToPass
+            }
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.tableData.count;
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
+        cell.textLabel?.text = tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let indexPath = tableView.indexPathForSelectedRow!;
+        let currentCell = tableView.cellForRowAtIndexPath(indexPath) as UITableViewCell!;
+        
+        valueToPass = currentCell.textLabel!.text
+        performSegueWithIdentifier("detailSegue", sender: self)
+    }
+    
+    @IBAction func addItem(sender: AnyObject) {
+        if (tableData[0] == "1. Sample List Item") {
+            tableData.removeAll()
+            tableData.append(String(listCount) + ". " + textField.text! as String!)
+        }
+        else {
+            listCount++
+            tableData.append(String(listCount) + ". " + textField.text! as String!)
+        }
+        tableView.reloadData()
+        
+    }
+    
+}
